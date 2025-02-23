46c231293219f87836e6e816177bc41ffe095b97
0xZensh <txr1883@gmail.com>
2025-02-23 17:47:56 +0800

    feat: add RPC command to `anda_engine_cli`

--- anda_engine_cli/src/main.rs
+++ anda_engine_cli/src/main.rs
@@ -17,13 +17,37 @@ struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Generate random bytes with the given length and format
     RandBytes {
+        /// Length of the random bytes, default is 32
         #[arg(short, long, default_value = "32")]
         len: usize,
-
+        /// Output format: hex or base64, default is hex
         #[arg(short, long, default_value = "hex")]
         format: String,
     },
+
+    /// make an signed RPC call to the endpoint with the given ICP identity, method and args.
+    /// The RPC response from the endpoint should be string.
+    /// Example: `anda_engine_cli rpc -i ./identity.pem -e 'https://andaicp.anda.bot/proposal'  -m start_x_bot`
+    Rpc {
+        #[arg(short, long, default_value = "http://127.0.0.1:8042/default")]
+        endpoint: String,
+
+        /// Path to ICP identity pem file or 32 bytes identity secret in hex.
+        #[arg(short, long, env = "ID_SECRET", default_value = "Anonymous")]
+        id_secret: String,
+
+        /// RPC method name
+        #[arg(short, long)]
+        method: String,
+
+        /// RPC arguments, default is []
+        #[arg(short, long)]
+        args: Option<Vec<String>>,
+    },
+
+    /// Run an AI agent with the given prompt and name on the endpoint.
     AgentRun {
         #[arg(short, long, default_value = "http://127.0.0.1:8042/default")]
         endpoint: String,
@@ -38,6 +62,8 @@ pub enum Commands {
         #[arg(short, long)]
         name: Option<String>,
     },
+
+    /// Call a tool with the given name and args on the endpoint.
     ToolCall {
         #[arg(short, long, default_value = "http://127.0.0.1:8042/default")]
         endpoint: String,
@@ -75,6 +101,27 @@ async fn main() -> Result<(), BoxError> {
                     println!("{:?}", bytes);
                 }
             }
+        }
+
+        Some(Commands::Rpc {
+            endpoint,
+            id_secret,
+            method,
+            args,
+        }) => {
+            let identity = load_identity(id_secret)?;
+            let web3 = Web3Client::builder()
+                .with_ic_host(&cli.ic_host)
+                .with_identity(Arc::new(identity))
+                .with_allow_http(true, None)
+                .build()
+                .await?;
+
+            println!("principal: {}", web3.get_principal());
+            let args = args.clone().unwrap_or_default();
+
+            let res: String = web3.https_signed_rpc(endpoint, method, &args).await?;
+            println!("{}", res);
         }
 
         Some(Commands::AgentRun {
